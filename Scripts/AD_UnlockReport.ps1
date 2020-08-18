a456802cbf598cdc5ea1787282119d182d61f9c7
Bruce Jernell <59374529+opconWizard@users.noreply.github.com>
2020-08-18 13:03:41 -0500

    Bug fixes
    Fixed a few bugs and added an option to include a Self Service link to a button for unlocks

--- Scripts/AD_UnlockReport.ps1
+++ Scripts/AD_UnlockReport.ps1
@@ -5,21 +5,26 @@
     ,$user     # Email user
     ,$password # Email password (use Encrypted Global Property)
     ,$port = 587 # Port for sending email
-    ,[switch]$debug
+    ,$selfserviceLink  # Example: https://<OpCon Server>:8443/#!selfservice/servicerequest/AD%20Unlock
 )
 
 try
 { 
     Get-Date | Out-Host
-    $results = get-aduser -filter * -Properties * | where-object{ $_.enabled -eq $true -and $_.lockedout -eq $true} | Select-Object Name,Mail,Enabled,LockedOut,PasswordExpired | Out-Host
-    $results | ForEach-Object{ $body = $body + "<br>Name: " + $_.Name + "<br>Mail: " + $_.Mail + "<br>Enabled: " + $_.Enabled + "<br>LockedOut: " + $_.LockedOut + "<br>PasswordExpired: " + $_.PasswordExpired + "<br>" }
-    $subject = "AD locked accounts report " + (Get-Date -Format "MM/dd/yyyy hh:mm")
+    $results = get-aduser -filter * -Properties * | where-object{ $_.enabled -eq $true -and $_.lockedout -eq $true} | Select-Object Name,Mail,Enabled,LockedOut,PasswordExpired
+    $results | Out-Host 
+    Write-host "Found"$results.Count"accounts that are locked out."
+
+    if($results.Count -gt 0) 
+    {
+        $results | ForEach-Object{ $body = $body + "<br>Name: " + $_.Name + "<br>Mail: " + $_.Mail + "<br>Enabled: " + $_.Enabled + "<br>LockedOut: " + $_.LockedOut + "<br>PasswordExpired: " + $_.PasswordExpired + "<br>" }
+        $subject = "AD locked accounts report " + (Get-Date -Format "MM/dd/yyyy hh:mm")
  
-    $secpasswd = ConvertTo-SecureString "$password" -AsPlainText -Force
-    $mycreds = New-Object System.Management.Automation.PSCredential ($user, $secpasswd)
+        $secpasswd = ConvertTo-SecureString "$password" -AsPlainText -Force
+        $mycreds = New-Object System.Management.Automation.PSCredential ($user, $secpasswd)
         
-    if(!$debug)
-    { Send-MailMessage -SmtpServer $smtp -To $to -From $from -Subject $subject -Body $body -BodyAsHtml -UseSSL -Credential $mycreds -Port $port }
+        Send-MailMessage -SmtpServer $smtp -To $to -From $from -Subject $subject -Body $body -BodyAsHtml -UseSSL -Credential $mycreds -Port $port
+    }
 }
 catch [Exception]
 {
